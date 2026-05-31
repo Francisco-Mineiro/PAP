9db4cc7eb4a5d745fba2e84814b838000b391fc5
Francisco-Mineiro <franciscomineiro68@gmail.com>
2026-05-31 14:12:49 +0100

    Fazer a ia funcionar fora da minha rede de casa

--- poupa/app/barra/IA.jsx
+++ poupa/app/barra/IA.jsx
@@ -36,7 +36,7 @@ export default function AIChat() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://192.168.1.9:3000/ai", {
+      const response = await fetch("https://pap-lcm3.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: currentInput })
